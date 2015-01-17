5bd85d91a00b85ecd97056aff1b90e9aafe66597
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-17 09:51:25 +0000

    - Removed SIGNATURE file. - Updated MANIFEST.SKIP file to ignore folder 'eumm/'. - Updated Copyright year information.

--- lib/Lingua/IND/Numbers.pm
+++ lib/Lingua/IND/Numbers.pm
@@ -1,6 +1,6 @@
 package Lingua::IND::Numbers;
 
-$Lingua::IND::Numbers::VERSION = '0.05';
+$Lingua::IND::Numbers::VERSION = '0.06';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Lingua::IND::Numbers - Indian Numbering System representation
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
@@ -272,7 +272,7 @@ L<http://search.cpan.org/dist/Lingua-IND-Numbers/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2014 Mohammad S Anwar.
+Copyright (C) 2014 - 2015 Mohammad S Anwar.
 
 This  program  is  free software; you can redistribute it and/or modify it under
 the  terms  of the the Artistic License (2.0). You may obtain a copy of the full
